6ed6ac838888554c0a24cfca09563f4e34085ccf
Shashank <shashankhey1710@gmail.com>
2022-10-30 13:32:18 +0530

    lesson 6

--- app/src/main/java/com/example/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/swoosh/WelcomeActivity.kt
@@ -6,7 +6,8 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_welcome.*
 
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
