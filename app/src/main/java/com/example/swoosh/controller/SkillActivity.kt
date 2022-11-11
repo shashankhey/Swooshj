eb342ef254f38836fae8aaf42fa07b2f440259fd
Shashank <shashankhey1710@gmail.com>
2022-11-11 20:23:07 +0530

    lesson 10

--- app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/example/swoosh/controller/SkillActivity.kt
@@ -12,11 +12,22 @@ import kotlinx.android.synthetic.main.activity_skill.*
 class SkillActivity : BaseActivity() {
 
     lateinit var player : Player
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)!!
-//        println(league)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) {
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        }
     }
 
     fun onBallerClick(view: View) {
